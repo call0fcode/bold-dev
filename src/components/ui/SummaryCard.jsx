fe28197a449168f914aa0462e3c33f0184eecc70
Juan Gómez Carrillo <gomezcarrillojuan@gmail.com>
2022-04-23 19:23:48 -0500

    ♻ refactor: Use semantic HTML
    - Use header tag instead of just a div in order to give the card header a
    more semantic meaning.

--- src/components/ui/SummaryCard.jsx
+++ src/components/ui/SummaryCard.jsx
@@ -7,10 +7,10 @@ import classes from "../../scss/components/ui/SummaryCard.module.scss";
 const SummaryCard = ({ date, totalAmount }) => {
   return (
     <div className={classes.card}>
-      <div className={classes.title}>
+      <header className={classes.title}>
         <p>Total de ventas de septiembre</p>
         <InfoIcon />
-      </div>
+      </header>
       <div className={classes.content}>
         <p className={classes["total-amount"]}>${totalAmount}</p>
         <p className={classes.date}>Septiembre 2021</p>
